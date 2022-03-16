1a13f95350d7f29c2bf3a8d2be8dd2f4b0c6e973
francisco-neto1 <99516124+francisco-neto1@users.noreply.github.com>
2022-03-16 20:01:09 -0300

    Update index.php

--- Lista2/Exercicio1/index.php
+++ Lista2/Exercicio1/index.php
@@ -1,72 +1,48 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Exercício 1</title>
-  </head>
-  <body class="container">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <title>Exercício 1</title>
+</head>
+
+<body class="container">
   <h1>Exercício 1</h1>
-  <h4>Informe 10 valores</h4>
+  <h4>Exercicio 1: Informe 10 valores</h4>
   <form method="post" action="resultado.php">
-    <div class="">
-      <div class="">
-        <label from="valor" class="label-control">Informe o valor: </label>
-        <input type="number" name="valor" id="valor" class="form-control" />
-      </div>
-      <div class="">
-        <label from="valor2" class="label-control">Informe o valor: </label>
-        <input type="number" name="valor2" id="valor2" class="form-control" />
-      </div>
-      <div class="col">
-        <label from="valor3" class="label-control">Informe o valor: </label>
-        <input type="number" name="valor3" id="valor3" class="form-control" />
-      </div>
-      <div class="col">
-        <label from="valor4" class="label-control">Informe o valor: </label>
-        <input type="number" name="valor4" id="valor4" class="form-control" />
-      </div>
-      <div class="col">
-        <label from="valor5" class="label-control">Informe o valor: </label>
-        <input type="number" name="valor5" id="valor5" class="form-control" />
-      </div>
-      <div class="col">
-        <label from="valor6" class="label-control">Informe o valor: </label>
-        <input type="number" name="valor6" id="valor6" class="form-control" />
-      </div>
-      <div class="col">
-        <label from="valor7" class="label-control">Informe o valor: </label>
-        <input type="number" name="valor7" id="valor7" class="form-control" />
-      </div>
-      <div class="col">
-        <label from="valor8" class="label-control">Informe o valor: </label>
-        <input type="number" name="valor8" id="valor8" class="form-control" />
-      </div>
-      <div class="col">
-        <label from="valor9" class="label-control">Informe o valor: </label>
-        <input type="number" name="valor9" id="valor9" class="form-control" />
-      </div>
-      <div class="col">
-        <label from="valor10" class="label-control">Informe o valor: </label>
-        <input type="number" name="valor10" id="valor10" class="form-control" />
-      </div>
+    <div class="row">
+
+      <?php //para fazer a repetição 
+      for ($i = 1; $i <= 10; $i++) {
+      ?>
     </div>
-    <div class="row mt-5">
-      <div class="col">
-        <button type="submit" class="btn btn-danger">Enviar</button>
+        <div class="col mt-2">
+          <label for="valor<?= $i?>" class="label-control">Informe o valor <?= $i ?>: </label>
+          <input type="number" name="valor" id="valor<?= $i?> " class="form-control" />
       </div>
+      <?php //seleciona oque quer ser repetito
+      }
+  ?>
+
+  <div class="row mt-5 md-4">
+    <div class="col">
+
+      <button type="submit" class="btn btn-primary">Enviar</button>
     </div>
+  </div>
   </form>
-    <!-- Optional JavaScript; choose one of the two! -->
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
+  <!-- Optional JavaScript; choose one of the two! -->
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <!-- Option 2: Separate Popper and Bootstrap JS -->
+  <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     -->
-  </body>
+</body>
+
 </html>
