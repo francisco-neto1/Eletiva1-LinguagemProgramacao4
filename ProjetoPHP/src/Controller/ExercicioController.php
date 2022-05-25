694fb14281fffaef82edabdd81eec54d7b95d298
francisco-neto1 <franciscon3t02018@hotmail.com>
2022-05-25 19:35:25 -0300

    ,

--- ProjetoPHP/src/Controller/ExercicioController.php
+++ ProjetoPHP/src/Controller/ExercicioController.php
@@ -1,10 +1,21 @@
 <?php
+
+
 namespace Aluno\ProjetoPhp\Controller;
+
 
 class ExercicioController
 {
-    public static function exibir()
-    {
-        require_once("../scr/View/exemplo.php");
+
+    public static function exibir(){
+        require_once("../src/View/exemplo.php");
+    }
+
+
+    public static function exibirResultado(){
+        $valor1 = $_POST['valor1'];
+        $valor2 = $_POST['valor2'];
+        $soma = $valor1 + $valor2;
+        require_once("../src/View/exemplo.php");
     }
 }
