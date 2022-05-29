38f889ad66fec44cc689b4caaa59132cc5810869
francisco-neto1 <franciscon3t02018@hotmail.com>
2022-05-29 10:05:31 -0300

    +

--- Lista1_Refeita/Exercicios/src/Controller/Ex4.php
+++ Lista1_Refeita/Exercicios/src/Controller/Ex4.php
@@ -6,26 +6,20 @@ namespace Aluno\Francisco\Controller;
 
 class Ex4
 {
-
     public static function exibir()
     {
         require_once("../src/View/exercicio4.php");
     }
-
-
     public static function exibirResultado()
     {
         $valor = $_POST['valor'];
 
         if ($valor > 0)
-            $result = "O valor informado é positivo";
-
+            $result = "O valor é positivo";
         if ($valor < 0)
-            $result = "O valor informado é negativo";
-
+            $result = "O valor é negativo";
         if ($valor == 0)
-            $result = "O valor informado é igual a 0";
-
+            $result = "O valor é igual a 0";
 
         require_once("../src/View/exercicio4.php");
     }
