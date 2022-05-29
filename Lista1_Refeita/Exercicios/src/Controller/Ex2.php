e7ca471346f5eebdff91e6df43c84f84a0144607
francisco-neto1 <franciscon3t02018@hotmail.com>
2022-05-29 10:00:34 -0300

    Update Ex2.php

--- Lista1_Refeita/Exercicios/src/Controller/Ex2.php
+++ Lista1_Refeita/Exercicios/src/Controller/Ex2.php
@@ -6,13 +6,10 @@ namespace Aluno\Francisco\Controller;
 
 class Ex2
 {
-
     public static function exibir()
     {
         require_once("../src/View/exercicio2.php");
     }
-
-
     public static function exibirResultado()
     {
         $valor = $_POST['valor'];
